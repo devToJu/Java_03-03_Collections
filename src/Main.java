f5b984744a4275ac09f9bab15ffcdb6669e2186f
devtoju <msg.toju@outlook.de>
2023-02-05 14:10:51 +0100

    add items separately and display the whole collection

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,20 @@ import java.util.Collection;
 public class Main {
     public static void main(String[] args) {
         Collection<String> collection = new ArrayList<>();
+        collection.add("A");
+        collection.add("B");
+        collection.add("C");
+
+        displayAllItems(collection);
+    }
+
+    private static <T> void displayAllItems(Collection<T> collection) {
+        // Using a foreach-Loop
+        for (var item : collection) {
+            System.out.println(item);
+        }
+
+        // Using the overwritten toString()-Method
+        System.out.println(collection);
     }
 }
