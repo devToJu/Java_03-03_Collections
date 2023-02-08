b1571e8b45a12f3e2179c9cb8a5d77f0eb2a73fe
devtoju <msg.toju@outlook.de>
2023-02-08 17:29:57 +0100

    Try to sort the list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Tom"));
+        users.add(new User("Jenny"));
+        users.add(new User("Hans"));
+        users.add(new User("Jenna"));
 
+        // Compilation error: user doesn't implement the comparable interface
+        // Collections.sort(users);
     }
 }
