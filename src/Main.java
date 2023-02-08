b70c9f7175be7bdda8b0e03d5a74eba3100c2a47
devtoju <msg.toju@outlook.de>
2023-02-08 17:25:24 +0100

    Init sorting with comparable interface

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<String>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
+
     }
 }
