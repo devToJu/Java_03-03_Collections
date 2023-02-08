c79603a7efa439fa1b6cb1c2c8522f339156c400
devtoju <msg.toju@outlook.de>
2023-02-08 16:02:20 +0100

    insert item to list at specified index

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Main {
+    private static List<String> list = new ArrayList<>();
+
+    // static initializer block
+    static {
+        Collections.addAll(list, "b" , "c");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(list);
+        list.add(0, "a");
+        System.out.println(list);
     }
 }
