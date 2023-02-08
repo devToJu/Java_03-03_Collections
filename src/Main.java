bc3ad03b0ae432ada30d76e263b5d538c6f46135
devtoju <msg.toju@outlook.de>
2023-02-08 16:13:26 +0100

    get element from list at specified index

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Main {
-    private static List<String> list = new ArrayList<>();
+    private static final List<String> list = new ArrayList<>();
 
     // static initializer block
     static {
@@ -11,8 +11,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        printList();
+        addElementAt(0);
+        printList();
+
+        System.out.println("Element at Index 1 is " + list.get(1));
+    }
+
+    private static void printList() {
         System.out.println(list);
-        list.add(0, "a");
-        System.out.println(list);
+    }
+
+    private static void addElementAt(int index) {
+        list.add(index, "a");
     }
 }
