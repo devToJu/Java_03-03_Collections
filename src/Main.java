b1405d3799ea04e9418c618d5f71614d48822e72
devtoju <msg.toju@outlook.de>
2023-02-11 14:25:44 +0100

    add() and offer() with limeted-size queues

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,5 +11,22 @@ public class Main {
         queue.add("rear");
         queue.offer("another rear");
         System.out.println(queue);
+
+        queueWithLimitedSize();
+    }
+
+    private static void queueWithLimitedSize() {
+        Queue<String> queue = new ArrayBlockingQueue<>(2, true, Arrays.asList("a", "b"));
+
+        try {
+            queue.add("c");
+        }
+        catch (IllegalStateException ex) {
+            System.out.println("Queue is full");
+        }
+
+        if (!queue.offer("c")) {
+            System.out.println("Queue is full");
+        }
     }
 }
