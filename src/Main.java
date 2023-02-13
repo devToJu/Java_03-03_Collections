78b3cf1079e3c5a69bdc9fe456d7da10d9913311
devtoju <msg.toju@outlook.de>
2023-02-13 13:43:27 +0100

    Add elements unsorted and remove duplicates

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,15 @@ public class Main {
     static Set<String> unsortedUniqueCollection = new HashSet<>();
     public static void main(String[] args) {
         System.out.println(unsortedUniqueCollection);
+
+        addItems();
+        System.out.println(unsortedUniqueCollection);
+    }
+
+    private static void addItems() {
+        unsortedUniqueCollection.add("sky");
+        unsortedUniqueCollection.add("is");
+        unsortedUniqueCollection.add("blue");
+        unsortedUniqueCollection.add("blue");
     }
 }
