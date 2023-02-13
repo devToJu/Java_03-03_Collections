415940f9c7f83481bfe8c18b3c70161d63a3d6f4
devtoju <msg.toju@outlook.de>
2023-02-13 17:45:12 +0100

    store users in a map

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,19 @@ import java.util.Map;
 public class Main {
 
     static Map<String, User> map = new HashMap<>();
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println(map);
+        storeUser();
+    }
+
+    private static void storeUser() {
+        var userA = new User("Tom", "e1");
+        var userB = new User("Tina", "e2");
+
+        map.put(userA.getEmail(), userA);
+        map.put(userB.getEmail(), userB);
+
+        System.out.println(map);
     }
 }
